b156a42a8a1fd6eb20c845170a76c05209131d0d
Putra Prima Arhandi <putraprima@gmail.com>
2020-07-31 00:04:20 +0700

    start reward dao

---
+++ app/src/main/java/id/putraprima/keluargakolaborasi/ui/reward/Reward.kt
@@ -0,0 +1,15 @@
+package id.putraprima.keluargakolaborasi.ui.reward
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "reward")
+data class Reward (
+    @PrimaryKey(autoGenerate = true)
+    var rewardId : Long = 0L,
+    @ColumnInfo(name = "rewardName")
+    var rewardName : String = "",
+    @ColumnInfo(name = "rewardPrice")
+    var rewardPrice : Long = 0
+)
---
+++ app/src/main/java/id/putraprima/keluargakolaborasi/ui/reward/RewardDao.kt
@@ -0,0 +1,25 @@
+package id.putraprima.keluargakolaborasi.ui.reward
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface RewardDao {
+    @Insert
+    fun insert(reward: Reward)
+
+    @Update
+    fun update(reward: Reward)
+
+    @Query("Select * from reward where rewardId = :key")
+    fun get(key:Long):Reward?
+
+    @Query("Delete From reward")
+    fun clear()
+
+    @Query("Select * from reward order by rewardId DESC")
+    fun getAllReward() : LiveData<List<Reward>>
+}
